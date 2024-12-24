83ea4f726fc20482f8844734ee08a2b5dbe51c63
0689436 <0689436@proton.me>
2024-12-24 16:41:24 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Toggle Dark and Light Mode
-const toggleModeButton = document.getElementById('toggle-mode');
-const body = document.body;
-
-toggleModeButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    // Toggle button icon
-    toggleModeButton.textContent = body.classList.contains('dark-mode') ? '🌞' : '🌙';
-});
+// script.js
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+}
